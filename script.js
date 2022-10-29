9937608fcb8caff7a94f4ca86eb88b623a25ab4a
Luiz Gustavo Machado <luiz.gustavo.machado@hotmail.com>
2022-10-29 14:28:41 -0300

    onclik open

--- script.js
+++ script.js
@@ -3,23 +3,32 @@ const form = document.querySelector("form")
 const inputWeight = document.querySelector("#weight")
 const inputHeight = document.querySelector("#height")
 
+const modalWrapper = document.querySelector('.modal-wrapper')
+const modalMessage = document.querySelector('.modal .title span')
+const modalBtnClose = document.querySelector('.close')
+
+
+
 form.onsubmit = function(event) {
     event.preventDefault()
-
+    
     const weight = inputWeight.value 
     const height = inputHeight.value
-
+    
     const result = IMC(weight, height)
-    console.log(result)
+    const message = `Seu IMC é de ${result}`
+  
+    modalMessage.innerText = message
+    modalWrapper.classList.add('open')
+}
+
+modalBtnClose.onclick = () => {
+    modalWrapper.classList.remove('open')
 }
 
 function IMC(weight, height) {
     return (weight / ((height / 100) **2)).toFixed(2)
-}   
-
-document.querySelector(".calcIMC")
-
-
+}
 
 
 
