898215ca638e13334626fb28b92c64afff710a69
Luiz Gustavo Machado <luiz.gustavo.machado@hotmail.com>
2022-11-10 15:58:38 -0300

    modulação

--- script.js
+++ script.js
@@ -3,31 +3,21 @@ const form = document.querySelector("form")
 const inputWeight = document.querySelector("#weight")
 const inputHeight = document.querySelector("#height")
 
-const modalWrapper = document.querySelector('.modal-wrapper')
-const modalMessage = document.querySelector('.modal .title span')
-const modalBtnClose = document.querySelector('.close')
-
-
-
-form.onsubmit = function(event) {
+form.onsubmit = function (event) {
     event.preventDefault()
-    
-    const weight = inputWeight.value 
+
+    const weight = inputWeight.value
     const height = inputHeight.value
-    
+
     const result = IMC(weight, height)
     const message = `Seu IMC é de ${result}`
-  
-    modalMessage.innerText = message
-    modalWrapper.classList.add('open')
-}
 
-modalBtnClose.onclick = () => {
-    modalWrapper.classList.remove('open')
+    Modal.message.innerText = message
+    Modal.open()
 }
 
 function IMC(weight, height) {
-    return (weight / ((height / 100) **2)).toFixed(2)
+    return (weight / ((height / 100) ** 2)).toFixed(2)
 }
 
 
